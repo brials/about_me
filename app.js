5a94e4d6e572b4222008f5b91ba726cea2cc124c
BrianAlspach <brian.alspach@outlook.com>
2016-11-29 14:56:09 -0800

    renamed user variable

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 'use strict';
 
-// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+var response1 = prompt('Does Sam have any cats?').toLowerCase();
 
-var question1 = prompt('Does Sam have any cats?').toLowerCase();
-
-// if( condition is true){
-//   do something
-//   maybe do something more
-//   etc. until done
-// } else {
-//   do this other thing
-//   and maybe yet another thing
-// }
-
-if (question1 === 'yes' || question1 === 'y'){
+if (response1 === 'yes' || response1 === 'y'){
   alert('yes, Sam does have three cats!')
-} else if(uestion1 === 'no' || question1 === 'n'){
+} else if(response1 === 'no' || response1 === 'n'){
   alert('Sorry, you\'re wrong, Sam has three cats.')
 }else {
   alert('you fail to understand yes/no questions. So that is as good as wrong. You lose.')
